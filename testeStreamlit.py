36b91c2765dd8ddd7941ea488e5f7083b2bf212d
Isabelly Souza <isabellysouza1998@gmail.com>
2023-02-09 16:09:08 -0300

    primeiro
    Co-authored-by: Nataly-Carvalho <natalycarvalho404@gmail.com>

---
+++ testeStreamlit.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.title('Pesquisadores do cesar is :orange[cool].')
+st.subheader('Projeto desenvolvido para o PET-Facep com parceria com o CESAR School')
+st.caption('Orientadores: Rafael Ferreira e Luana Cristina')
+st.write('O projeto se trata de extrair dados de curriculos de pesquisadores da Plataforma Lattes ultilizado o lucyLattes para automatizar o processo (O lucyLattes é programado em python), os dados que precisamos extrair foram os seguintes:')
+st.write('O projeto se trata de extrair dados de curriculos de pesquisadores da Plataforma Lattes ultilizado o lucyLattes para automatizar o processo (O lucyLattes é programado em python), os dados que precisamos extrair foram os seguintes:')
